b5f3405eb749e96d918a810b58ee99aeaa65a320
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-05-30 14:33:08 -0700

    Time: 83 ms (49.21%), Space: 42.1 MB (77.32%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    var stack = [];
+    
+    var op = ["(", "{", "["];
+    var cl = [")", "}", "]"];
+    
+    for(var i=0; i<s.length; i++) {
+        if(op.includes(s[i])) stack.push(s[i]);
+        else {
+            if(cl.indexOf(s[i]) != -1){
+                if(stack[stack.length-1] === op[cl.indexOf(s[i])]) stack.pop();
+                else return false;
+            }
+        }
+    }
+    if(stack.length != 0) return false;
+    return true;
+};
+
+// 1st Approach: if -> don't need to close in order.
+// var data = {
+//         "(" : 0,
+//         "{" : 0,
+//         "[" : 0
+//     }
+//     for(var i=0; i<s.length; i++) {
+//         if(s[i] == "(" || s[i] == "[" || s[i] == "{") data[s[i]]++;
+//         else if(s[i] == ")") data["("]--;
+//         else if(s[i] == "}") data["{"]--;
+//         else data["["]--;
+//     }
+//     if(data["("] != 0 || data["{"] != 0 || data["["] != 0) return false;
+//     return true;
