cb2360262a978d84c952657f38b1d61feb2bad87
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-06-02 13:19:52 -0700

    Time: 77 ms (53.14%), Space: 41.8 MB (84.77%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -6,14 +6,14 @@
 
 // Time: O(n) | Space: O(1)
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
-    // if(needle.length == 0) return 0;
-    // for(var i=0;i<haystack.length;i++) {
-    //     if(haystack.substr(i, needle.length) === needle) {
-    //         return i;
-    //     }
-    // }
-    // return -1;
+    if(needle.length == 0) return 0;
+    for(var i=0;i<haystack.length;i++) {
+        if(haystack.substr(i, needle.length) === needle) {
+            return i;
+        }
+    }
+    return -1;
 };
 // similar: 
 //     while(haystack.substr(i, needle.length) === needle && i<haystack.length) { i++; };
+// return haystack.indexOf(needle)
