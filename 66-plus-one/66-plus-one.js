64ffbfbf794431e58f3945a0040e013052cfd6d2
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-06-03 20:14:05 -0700

    Time: 95 ms (23.26%), Space: 42.3 MB (18.86%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -3,21 +3,5 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    var len = digits.length;
-    if(digits[len-1] + 1 >= 10) {
-        digits[len-1] = 0;
-        var i = len-2;
-        while(i>=0) {
-            if(digits[i]+1 >= 10) digits[i] = 0;
-            else {
-                digits[i] += 1;
-                return digits
-            }
-            i--;
-        }
-        if(digits[0] == 0) digits.unshift(1);
-    }
-    else digits[len-1] += 1;
-    
-    return digits;
+    return [...(BigInt(digits.join(""))+1n).toString()];
 };
