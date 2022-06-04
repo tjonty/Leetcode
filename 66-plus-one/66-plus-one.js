590fe3bcaec88ee48e5158d2b178ffb211f33767
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-06-03 20:15:19 -0700

    Time: 131 ms (5.05%), Space: 41.6 MB (96.02%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -2,6 +2,26 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+// Time: O(n) | Space: O(1)
 var plusOne = function(digits) {
-    return [...(BigInt(digits.join(""))+1n).toString()];
+    var len = digits.length;
+    if(digits[len-1] + 1 >= 10) {
+        digits[len-1] = 0;
+        var i = len-2;
+        while(i>=0) {
+            if(digits[i]+1 >= 10) digits[i] = 0;
+            else {
+                digits[i] += 1;
+                return digits
+            }
+            i--;
+        }
+        if(digits[0] == 0) digits.unshift(1);
+    }
+    else digits[len-1] += 1;
+    
+    return digits;
 };
+
+// Another Aproach from discussion
+// return [...(BigInt(digits.join(""))+1n).toString()];
