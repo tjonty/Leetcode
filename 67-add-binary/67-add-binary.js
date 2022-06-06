5701264906e3b8b011819481fedceb155436fa6f
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-06-06 15:39:58 -0700

    Time: 70 ms (85.31%), Space: 43.6 MB (46.24%) - LeetHub

--- 67-add-binary/67-add-binary.js
+++ 67-add-binary/67-add-binary.js
@@ -3,6 +3,29 @@
  * @param {string} b
  * @return {string}
  */
+// Time: O(?) || Space: O(?)
 var addBinary = function(a, b) {
-    return (BigInt('0b' + a)+BigInt('0b' + b)).toString(2);
+    var i = a.length-1;
+    var j = b.length-1;
+    var carry = 0;
+    var res = [];
+    
+    while(i>-1 || j>-1) {
+        var num_a = i>-1 ? parseInt(a[i]) : 0;
+        var num_b = j>-1 ? b.charAt(j) - "0" : 0;
+        var total = (num_a + num_b + carry).toString(2);
+        total = Number(total);
+        res.push(total % 10);
+        carry = Math.floor(total / 10);
+        i--;
+        j--;
+    }
+    
+    if(carry === 1) res.push("1");
+    
+    return res.reverse().join("");
+//     for large numbers
+// return (BigInt('0b' + a)+BigInt('0b' + b)).toString(2);
 };
+// for small numbers
+// return (parseInt(a,2)+parseInt(b,2)).toString(2))
