385f9a3127ac5503f6079717e80b8cc3775d0057
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-06-22 13:11:02 -0700

    Time: 128 ms (7.01%), Space: 42.4 MB (61.56%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.js
+++ 771-jewels-and-stones/771-jewels-and-stones.js
@@ -3,12 +3,22 @@
  * @param {string} stones
  * @return {number}
  */
+// Time: O(n*m) | n length of J; m length of S 
+// Space: O(1)
 var numJewelsInStones = function(jewels, stones) {
     let count = 0;
-    for(var i=0; i<jewels.length; i++) {
-        for(var j=0; j<stones.length; j++) {
+    
+    // for(var i=0; i<jewels.length; i++) {
+    //     for(var j=0; j<stones.length; j++) {
+    //         if(jewels[i] === stones[j]) count++;
+    //     }
+    // }
+    
+    for(var j=0; j<stones.length; j++) {
+        for(var i=0; i<jewels.length; i++) {
             if(jewels[i] === stones[j]) count++;
         }
     }
+    
     return count;
 };
