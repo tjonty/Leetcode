f02d2fb6cdc6accdac99df5de794833ce95b2f70
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-07-13 17:11:12 -0700

    Time: 170 ms (64.38%), Space: 44.5 MB (74.18%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -3,18 +3,21 @@
  * @return {number[]}
  */
 var minOperations = function(boxes) {
-    let idxones = [];
-    let answers = [];
+//     Time: O(n^2) | (n^2) + (n)
+//     Space: O(n) | (n) + (n)
+    let idxones = []; // S(n) - if all have the 1's 
+    let answers = []; // S(n) - must be n for all boxes
 
     // all the indexs of 1's
-    for(i=0;i<boxes.length;i++) {
+    for(i=0;i<boxes.length;i++) { // T(n) - for all boxes
         if(boxes[i] == "1") idxones.push(i);
     }
 
 //     calculating the operations & answers[]
-    for(i=0;i<boxes.length;i++) {
+    // T(n) - O(n) * O(n)
+    for(i=0;i<boxes.length;i++) { // T(n) - for all boxes
         let op = 0;
-        for(j=0;j<idxones.length;j++) {
+        for(j=0;j<idxones.length;j++) { // T(n) - if all have 1's
             let diff = Math.abs(idxones[j] - i);
             op += diff;
         }
