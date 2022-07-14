89182f5a20e4d3242016428ac05a56b7ab95d358
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-07-13 19:42:59 -0700

    Time: 125 ms (93.75%), Space: 44.2 MB (88.54%) - LeetHub

---
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @param {number[]} startPos
+ * @param {string} s
+ * @return {number[]}
+ */
+var executeInstructions = function(n, startPos, s) {
+    let answers = [];
+    for (i = 0; i < s.length; i++) {
+        let movement = 0;
+        let row = startPos[0];
+        let col = startPos[1];
+        for (j = i; j < s.length; j++) {
+            if (s[j] == "R") col++;
+            else if (s[j] == "L") col--;
+            else if (s[j] == "D") row++;
+            else row--;
+            if(row>n-1 || col > n-1 || row < 0 || col < 0) {
+                break;
+            }
+            movement++;
+        }
+        answers[i] = movement;
+    }
+    return answers;
+};
