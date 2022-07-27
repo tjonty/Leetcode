9a99ad44c4c14759dcef8be48145496fce74a474
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-07-26 22:04:56 -0700

    Time: 105 ms (57.84%), Space: 44.8 MB (63.34%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -3,18 +3,31 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    // Time: O(n) || Space: O(n)
-    var data = {};
     
-    for(const num of nums) {
-        if(!data[num])
-            data[num] = 1;
-        else
-            data[num]++;
-    }
-
-    for(const num in data) {
-        if(data[num] == 1)
-            return num;
+    if(nums.length == 1)
+        return nums[0];
+    
+    nums.sort((a,b) => a-b);
+    for(var i=0;i<nums.length;i++) {
+        if(nums[i] !== nums[i+1])
+            return nums[i];
+        else 
+            i++;
     }
 };
+
+// Soln: 1;
+// Time: O(n) || Space: O(n)
+//     var data = {};
+    
+//     for(const num of nums) {
+//         if(!data[num])
+//             data[num] = 1;
+//         else
+//             data[num]++;
+//     }
+
+//     for(const num in data) {
+//         if(data[num] == 1)
+//             return num;
+//     }
