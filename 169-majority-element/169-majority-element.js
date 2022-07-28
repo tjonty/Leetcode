62741fa8171cbe324f1faea7d87b31ee3535c0a5
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-07-27 19:13:37 -0700

    Time: 61 ms (99.06%), Space: 45.8 MB (16.56%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,20 +3,24 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    // soln: 2;
+    // Time: O(n logn) || Space: O(1)
+    nums.sort();
+    return nums[Math.floor(nums.length/2)];
     
-    var data = {};
-
-    for(const num of nums) {
-        if(!data[num])
-            data[num] = 1;
-        else 
-            data[num]++;
-    }
-
-    let division = Math.floor(nums.length/2);
-    for(var num in data) {
-        if(data[num] > division) {
-            return num;
-        }
-    }
 };
+
+// soln: 1;
+    // Time: O(n) || Space: O(n)
+//     var data = {};
+//     var division = Math.floor(nums.length/2);
+
+//     for(const num of nums) {
+//         if(!data[num])
+//             data[num] = 1;
+//         else 
+//             data[num]++;
+
+//         if(data[num] > division) 
+//             return num;
+//     }
