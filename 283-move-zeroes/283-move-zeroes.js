6fc65bf3d16640168c2fe31cf45525520105d5b7
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-07 19:55:24 -0700

    Time: 81 ms (99.39%), Space: 46.6 MB (59.31%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -2,27 +2,39 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-//     soln 1: O(n) Time | O(n) Space
-    let arr = [];
-    let countZeroes = 0;
+var moveZeroes = function(nums) { 
+//     soln 3: O(n) Time | O(1) Space
+    let lastNonZero = 0;
+    
     for(i=0;i<nums.length;i++) {
-        if(nums[i] == 0)
-            countZeroes++;
+        if(nums[i] != 0) {
+            nums[lastNonZero++] = nums[i];
+        }
     }
     
-    for(i=0;i<nums.length;i++) {
-        if(nums[i] != 0)
-            arr.push(nums[i]);
-    }  
-    
-    while(countZeroes != 0) {
-        arr.push(0);   
-        countZeroes--;
+    for(i=lastNonZero;i<nums.length;i++) {
+        nums[i] = 0;
     }
-    
-    for(i=0;i<nums.length;i++) {
-       nums[i] = arr[i];
-    }  
-    
 };
+
+//     soln 1: O(n) Time | O(n) Space
+//     let arr = [];
+//     let countZeroes = 0;
+//     for(i=0;i<nums.length;i++) {
+//         if(nums[i] == 0)
+//             countZeroes++;
+//     }
+    
+//     for(i=0;i<nums.length;i++) {
+//         if(nums[i] != 0)
+//             arr.push(nums[i]);
+//     }  
+    
+//     while(countZeroes != 0) {
+//         arr.push(0);   
+//         countZeroes--;
+//     }
+    
+//     for(i=0;i<nums.length;i++) {
+//        nums[i] = arr[i];
+//     }
