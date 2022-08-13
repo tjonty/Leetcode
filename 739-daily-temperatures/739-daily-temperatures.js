ca867a160a081b34f834bd620a133d85e797e6f0
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-12 21:01:27 -0700

    Time: 381 ms (56.57%), Space: 74.8 MB (12.05%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.js
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -4,22 +4,38 @@
  */
 var dailyTemperatures = function(temperatures) {
 //     Time: O(n^2) | Space: O(n)
-    let answers = [];
-    let count = 0, j = 0;
+//     let answers = [];
+//     let count = 0, j = 0;
     
-    for(let i=0; i<temperatures.length; i++) {
-        count = 0;
-        for(let j=i; j<temperatures.length; j++) {
+//     for(let i=0; i<temperatures.length; i++) {
+//         count = 0;
+//         for(let j=i; j<temperatures.length; j++) {
          
-            if(temperatures[j] > temperatures[i] ){
-                break;
-            } else{
-                count++;
-            }   
-            if(j==temperatures.length - 1)
-                count=0;
+//             if(temperatures[j] > temperatures[i] ){
+//                 break;
+//             } else{
+//                 count++;
+//             }   
+//             if(j==temperatures.length - 1)
+//                 count=0;
+//         }
+//         answers.push(count);
+//     }
+//     return answers;
+    
+    const len = temperatures.length;
+    
+    const stack = [];
+    const ans = new Array(len).fill(0);
+    for(let i = 0; i < len; i++) {
+        if(i == 0) stack.push([temperatures[i], i]);
+        else {
+            while(stack.length && stack.at(-1)[0] < temperatures[i]) {
+                const prevIdx = stack.pop()[1];
+                ans[prevIdx] = i - prevIdx; 
+            }
+            stack.push([temperatures[i], i]);
         }
-        answers.push(count);
     }
-    return answers;
+    return ans;
 };
