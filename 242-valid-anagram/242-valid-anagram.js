908274f1e337888cc0b02194773135889c07d7e3
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-21 16:39:14 -0700

    Time: 124 ms (51.95%), Space: 45.6 MB (39.38%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,21 +4,32 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-        // soln 1 Time O(n) Space O(n)
-    var data_s = {};
-    var data_t = {};
-    
+    var arrS = [...s];
+    var arrT = [...t];
     if(s.length != t.length) return false;
 
-    for(var i=0;i<s.length;i++){
-        data_s[s[i]] ? data_s[s[i]]++ : data_s[s[i]] = 1;
-        data_t[t[i]] ? data_t[t[i]]++ : data_t[t[i]] = 1;
-    }
+    arrS.sort();arrT.sort();
 
-    for(var i in data_s){
-        if(data_s[i] != data_t[i])
+    for(var i=0;i<s.length;i++){
+        if(arrS[i] != arrT[i]) 
             return false;
     }
-
     return true;
+        // soln 1 Time O(n) Space O(n+n)
+//     var data_s = {};
+//     var data_t = {};
+    
+//     if(s.length != t.length) return false;
+
+//     for(var i=0;i<s.length;i++){
+//         data_s[s[i]] ? data_s[s[i]]++ : data_s[s[i]] = 1;
+//         data_t[t[i]] ? data_t[t[i]]++ : data_t[t[i]] = 1;
+//     }
+
+//     for(var i in data_s){
+//         if(data_s[i] != data_t[i])
+//             return false;
+//     }
+
+//     return true;
 };
