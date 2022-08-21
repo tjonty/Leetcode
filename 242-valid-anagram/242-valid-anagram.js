5395342dfb59f3517b1efed9b3fb0dfe4c7eeb06
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-21 16:40:34 -0700

    Time: 98 ms (81.59%), Space: 43 MB (82.22%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,32 +4,34 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    var arrS = [...s];
-    var arrT = [...t];
+    // soln 1 Time O(S) Space O(S+T)
+    var data_s = {};
+    var data_t = {};
+    
     if(s.length != t.length) return false;
 
-    arrS.sort();arrT.sort();
-
     for(var i=0;i<s.length;i++){
-        if(arrS[i] != arrT[i]) 
+        data_s[s[i]] ? data_s[s[i]]++ : data_s[s[i]] = 1;
+        data_t[t[i]] ? data_t[t[i]]++ : data_t[t[i]] = 1;
+    }
+
+    for(var i in data_s){
+        if(data_s[i] != data_t[i])
             return false;
     }
+
     return true;
-        // soln 1 Time O(n) Space O(n+n)
-//     var data_s = {};
-//     var data_t = {};
-    
+};
+
+//     Soln 2: Time O(nlogn) Space(1) Not optimal
+//     var arrS = [...s];
+//     var arrT = [...t];
 //     if(s.length != t.length) return false;
 
-//     for(var i=0;i<s.length;i++){
-//         data_s[s[i]] ? data_s[s[i]]++ : data_s[s[i]] = 1;
-//         data_t[t[i]] ? data_t[t[i]]++ : data_t[t[i]] = 1;
-//     }
+//     arrS.sort();arrT.sort();
 
-//     for(var i in data_s){
-//         if(data_s[i] != data_t[i])
+//     for(var i=0;i<s.length;i++){
+//         if(arrS[i] != arrT[i]) 
 //             return false;
 //     }
-
 //     return true;
-};
