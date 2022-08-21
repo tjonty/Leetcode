330084aa2dbf6d629653a8adcf9f3e70717d2343
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-21 16:06:54 -0700

    Time: 76 ms (90.85%), Space: 44.3 MB (70.05%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,27 +3,33 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    // Soln 2: T O(n) S O(1)
+    var idxSum = nums.length;
+    for(var i=0;i<nums.length;i++){
+        idxSum += (i-nums[i])
+    }
+    return idxSum;
 //     soln 1: Time O(n) Space O(n) Not optimal
-    var data = {};
-    var max = -1;
-    var i = 0;
+//     var data = {};
+//     var max = -1;
+//     var i = 0;
 
-    for( var n of nums){
-        max = Math.max(max, n);
-    }
+//     for( var n of nums){
+//         max = Math.max(max, n);
+//     }
 
-    while(i<=nums.length) {
-        data[i] = 0;
-        i++;
-    }
+//     while(i<=nums.length) {
+//         data[i] = 0;
+//         i++;
+//     }
 
-    for(var i=0;i<nums.length;i++) {
-        data[nums[i]]++;
-    }
+//     for(var i=0;i<nums.length;i++) {
+//         data[nums[i]]++;
+//     }
 
-    for(var i in data){
-        if(data[i] == 0){
-            return i;
-        }
-    }
+//     for(var i in data){
+//         if(data[i] == 0){
+//             return i;
+//         }
+//     }
 };
