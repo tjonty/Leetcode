dff70ed66309053e263d161bcbacc588e5c5ed92
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-23 13:15:34 -0700

    Time: 89 ms (87.95%), Space: 46.8 MB (37.89%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -4,23 +4,50 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    var data = {};
-    var res = [];
+    // Soln 2 Time O(n) Space O(n)
+    var data = {}; // O(n)
+    var freq = []; // O(n)
+    var res = []; // O(k)
 
-    for(var num of nums) { // Time O(n)
-        data[num] ? data[num]++ : data[num]=1;
+    for(var i=0;i<=nums.length;i++) // O(n)
+        freq[i] = [];
+
+    for(var num of nums) { // O(n)
+        data[num] ? data[num]++ : data[num] = 1;
     }
 
-    var keys = Object.keys(data);
-    var idx = keys.length-1;
-
-    keys.sort((a,b) => data[a] - data[b]); // Time O(n logn)
-
-    while(k >=1){
-        res.push(keys[idx]);
-        idx--;
-        k--;
+    for(var num in data) { // O(n)
+        freq[data[num]].push(num);
     }
-
-    return res;
+    
+    // No O(n^2) Always run =k so O(k)
+    for(var i=freq.length-1;i>=0;i--) {
+        for(var num of freq[i]) {
+            res.push(num);
+            if(res.length == k)
+                return res;
+        }
+    }
+    
 };
+
+// Soln 1: Time O(n logn) Space O(n) Not optimal, as follow up.
+//     var data = {};
+//     var res = [];
+
+//     for(var num of nums) { // Time O(n)
+//         data[num] ? data[num]++ : data[num]=1;
+//     }
+
+//     var keys = Object.keys(data);
+//     var idx = keys.length-1;
+
+//     keys.sort((a,b) => data[a] - data[b]); // Time O(n logn)
+
+//     while(k >=1){
+//         res.push(keys[idx]);
+//         idx--;
+//         k--;
+//     }
+
+//     return res;
