25dde66b654fef44ab32547e803fad9e5eeedfa3
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-25 12:18:43 -0700

    Time: 731 ms (17.90%), Space: 57.4 MB (70.38%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,29 +3,51 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    
-//     Soln 2: Time O(nlogn) Space O(1)
-    if(nums.length == 0) return 0; 
-    
-    var current_streak = 1;
-    var longest_streak = 1;
+    // soln 3: Time O(n) Space O(n)
+    var current_streak = 1, current_num;
+    var longest_streak = 0;
+    var nums_set = new Set(nums);
 
-    nums.sort((a, b) => a - b);
-
-    for(var i=1;i<nums.length;i++) {
-        if(nums[i] != nums[i-1]) {
-            if(nums[i-1] == nums[i]-1){
+    for(var num of nums) {
+        if(!nums_set.has(num-1)) {
+            current_num = num;
+            current_streak = 1
+            while(nums_set.has(current_num+1)){
                 current_streak++;
+                current_num++;
             }
-            else {
-                longest_streak = Math.max(current_streak, longest_streak);
-                current_streak = 1;
-            }
+            longest_streak = Math.max(current_streak, longest_streak);
         }
     }
 
-    return Math.max(current_streak, longest_streak);
+    return longest_streak;
+
+    
+    
+
 };
+
+//     Soln 2: Time O(nlogn) Space O(1)
+//     if(nums.length == 0) return 0; 
+    
+//     var current_streak = 1;
+//     var longest_streak = 1;
+
+//     nums.sort((a, b) => a - b);
+
+//     for(var i=1;i<nums.length;i++) {
+//         if(nums[i] != nums[i-1]) {
+//             if(nums[i-1] == nums[i]-1){
+//                 current_streak++;
+//             }
+//             else {
+//                 longest_streak = Math.max(current_streak, longest_streak);
+//                 current_streak = 1;
+//             }
+//         }
+//     }
+
+//     return Math.max(current_streak, longest_streak);
 
 //     Soln 1: Time O(n^3) Space O(1)
 //     Not worked - Time Limit Exceeded
