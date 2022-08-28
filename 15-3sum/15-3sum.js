8b3184c6d061f404c7d085f954e1daa1160b2d51
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-08-27 20:22:59 -0700

    Time: 134 ms (97.51%), Space: 53.2 MB (35.62%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    var answers = [];
+    var [l,r] = [0,0];
+    nums.sort((a,b) => a-b);
+    for(var i=0;i<nums.length-1;i++) {
+        [l,r] = [i+1, nums.length-1];
+        if(i>0 && nums[i] == nums[i-1]){
+            continue;
+        }
+        while(l<r) {
+            if(nums[l] + nums[r] + nums[i] == 0) {
+                answers.push([nums[i],nums[l],nums[r]]);
+                l++;
+                while(l<r && nums[l] == nums[l-1]) l++;
+            }
+            else if(nums[l] + nums[r] + nums[i] > 0){
+                r--;
+            } else {
+                l++;
+            }
+        }
+    }
+    return answers;
+};
