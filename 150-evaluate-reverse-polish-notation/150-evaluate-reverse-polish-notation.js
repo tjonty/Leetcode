155c53496d0e89c170c87d24b46f64c6fbb1999b
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-09-05 21:54:57 -0700

    Time: 119 ms (36.28%), Space: 44.5 MB (77.89%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -3,27 +3,49 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    var operator = ['+', "-", "/", "*"];
+    // Nice Way :::
+    var OPERATORS = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => Math.trunc(a / b),
+    };
     var stack = [];
-    for(var token of tokens) {
-        if(operator.includes(token) && stack.length != 0)
-            stack.push(action(stack.pop(), stack.pop(), token));
-        else 
-            stack.push(token)
+    for (var token of tokens) {
+        if (token in OPERATORS) {
+            var rhs = stack.pop();
+            var lhs = stack.pop();
+            stack.push(OPERATORS[token](lhs, rhs));
+        } else {
+            stack.push(Number(token));
+        }
     }
-
-    function action(a, b, op) {
-        a = Number(a);
-        b = Number(b);
-        if(op == "+")
-            return a+b;
-        else if(op == "-")
-            return b-a;
-        else if(op == "*")
-            return a*b;
-        else 
-            return Math.trunc(b/a);
-    }
-
-    return stack[0]
+    return stack.pop();
 };
+
+ // Soln 1: Worked but More Code:- 
+    // Time O(n) Space O(1)
+   
+//     var operator = ['+', "-", "/", "*"];
+//     var stack = [];
+//     for(var token of tokens) {
+//         if(operator.includes(token) && stack.length != 0)
+//             stack.push(action(stack.pop(), stack.pop(), token));
+//         else 
+//             stack.push(token)
+//     }
+
+//     function action(a, b, op) {
+//         a = Number(a);
+//         b = Number(b);
+//         if(op == "+")
+//             return a+b;
+//         else if(op == "-")
+//             return b-a;
+//         else if(op == "*")
+//             return a*b;
+//         else 
+//             return Math.trunc(b/a);
+//     }
+
+//     return stack[0]
