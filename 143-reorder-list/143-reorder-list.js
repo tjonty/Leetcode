5fa78154731ecc2c5990d0ff6deab00febe8f74c
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-09-15 23:34:08 -0700

    Time: 202 ms (9.92%), Space: 50.2 MB (51.40%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    // Find Middle
+    var [slow, fast] = [head, head.next];
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Reverse Second Half
+    second = slow.next;
+    prev = null;
+    slow.next = null;
+    while(second) {
+        tmp = second.next;
+        second.next = prev;
+        prev = second;
+        second = tmp;
+    }    
+
+    // Merge Two List
+    second = prev;
+    first = head;
+    while(second){
+        var [tmp1, tmp2] = [first.next, second.next];
+        first.next = second;
+        second.next = tmp1;
+        first = tmp1;
+        second = tmp2;
+    }
+};
