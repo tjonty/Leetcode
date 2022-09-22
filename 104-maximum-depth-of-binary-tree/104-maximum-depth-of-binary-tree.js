c08c37d6e2820b4388e2604ffa1c83536ba7f72f
Jonty Tejani <47101523+tjonty@users.noreply.github.com>
2022-09-21 20:18:22 -0700

    Time: 137 ms (7.97%), Space: 47.4 MB (5.08%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,11 +11,41 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    
-//     Soln 1 : Recursive DFS 
+//     Soln 3: Iterative DFS
+    //     Time : O(n) Space: O(h)::worst: O(n)
+    var stack = [[root, 1]];
+    var res = 0;
+    while(stack.length != 0) {
+        var [node ,depth] = stack.pop();
+        
+        if(node) {
+            res = Math.max(res, depth);
+            stack.push([node.left, depth + 1]);
+            stack.push([node.right, depth + 1]);
+        }
+    }
+    return res;
+//     Soln 2: Iterative BFS | no need to use BFS in this question
 //     Time : O(n) Space: O(h)::worst: O(n)
-    if(!root)
-        return 0;
+//     if(!root)
+//         return 0;
     
-    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+//     var q = [root];level=0;
+//     while(q.length != 0) {
+//         for(var val of q){
+//             var node = q.shift();
+//             if(node.left)
+//                 q.push(node.left);
+//             if(node.right)
+//                 q.push(node.right);
+//         }
+//         level++;
+//     }
+//     return level;
+//     Soln 1 : Recursive DFS | 45% 100ms
+//     Time : O(n) Space: O(h)::worst: O(n)
+//     if(!root)
+//         return 0;
+    
+//     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
